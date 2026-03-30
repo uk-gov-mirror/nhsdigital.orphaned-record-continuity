c47c10c665be7f96b1683da7e3208e81470f4a15
Kris Bloe <2674722+chrisbloe@users.noreply.github.com>
2026-03-30 14:01:51 +0100

    [PRM-764] Pin setuptools to 81.0.0 to fix build issues (#314)

--- services/mesh-forwarder/setup.py
+++ services/mesh-forwarder/setup.py
@@ -5,5 +5,5 @@ setup(
     version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=["boto3~=1.18", "mesh_client~=1.4"],
+    install_requires=["boto3~=1.18", "mesh_client~=1.4", "setuptools==81.0.0"],
 )
